9bdbe987c0c095765997e826668847a151ee246e
Danilo Vitoriano <vitoriano08@gmail.com>
2018-08-27 18:21:56 -0300

    add header and link

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Link from "./Link";
+
+function Header(props) {
+  return (
+    <div className="header">
+      <div>
+        <h1>Marketplace</h1>
+      </div>
+      <div>{!props.backButton ? null : <Link href="/" label="Voltar" />}</div>
+    </div>
+  );
+}
+
+export default Header;
